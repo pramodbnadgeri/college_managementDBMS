f8864a34a51f581e39fb520f4cf6c21a36772d20
pramod <49087662+pramodbnadgeri@users.noreply.github.com>
2019-03-30 11:33:34 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>::. Sir MVIT VTU UNIVERSITY.::</title>
+</head>
+
+<body>
+<div id="content">
+	<div >
+    		<img src="picture/mvit.jpg" width="800" height="420"  />
+    </div>
+    
+    <div id="space"></div>
+    
+    <div id="question">
+    
+    </div>
+    
+    <div id="space"></div>
+    
+    <div style="text-indent:40px; text-align:justify">
+    
+
+  </div>
+</div><!--end of content -->
+</body>
+</html>
---
+++ my.php
@@ -0,0 +1,5 @@
+<?php 
+echo "my name is pramod"
+
+?>
+shonaligaur54321@gmail.com is my id
